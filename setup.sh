e3c43f0e0b170ef3bcc0554e42e6dd585b54ca96
NEW-KJSVIP <newkjs09@gmail.com>
2025-08-31 12:04:07 +0700

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -24,7 +24,7 @@ green='\e[0;32m'
 TIME=$(date '+%d %b %Y')
 ipsaya=$(wget -qO- ipinfo.io/ip)
 TIMES="10"
-eval $(wget -qO- "https://raw.githubusercontent.com/kjsstore/kjs-new/main/Fls/botkey")
+eval $(wget -qO- "https://raw.githubusercontent.com/NEW-KJSVIP/kjs-new.v4/main/Fls/botkey")
 URL="https://api.telegram.org/bot$KEY/sendMessage"
 clear
 export IP=$( curl -sS icanhazip.com )
@@ -78,9 +78,9 @@ echo -e "\e[32mloading...\e[0m"
 clear
 clear
 rm -f /usr/bin/user
-username=$(curl https://raw.githubusercontent.com/kjsstore/izin/main/ip | grep $MYIP | awk '{print $2}')
+username=$(curl https://raw.githubusercontent.com/NEW-KJSVIP/izin-ip/main/ip | grep $MYIP | awk '{print $2}')
 echo "$username" >/usr/bin/user
-expx=$(curl https://raw.githubusercontent.com/kjsstore/izin/main/ip | grep $MYIP | awk '{print $3}')
+expx=$(curl https://raw.githubusercontent.com/NEW-KJSVIP/izin-ip/main/ip | grep $MYIP | awk '{print $3}')
 echo "$expx" >/usr/bin/e
 username=$(cat /usr/bin/user)
 exp=$(cat /usr/bin/e)
@@ -98,7 +98,7 @@ mai="datediff "$Exp" "$DATE""
 Info="(${green}Active${NC})"
 Error="(${RED}ExpiRED${NC})"
 today=`date -d "0 days" +"%Y-%m-%d"`
-Exp1=$(curl https://raw.githubusercontent.com/kjsstore/izin/main/ip | grep $MYIP | awk '{print $4}')
+Exp1=$(curl https://raw.githubusercontent.com/NEW-KJSVIP/izin-ip/main/ip | grep $MYIP | awk '{print $4}')
 if [[ $today < $Exp1 ]]; then
 sts="${Info}"
 else
@@ -265,8 +265,8 @@ fi
 }
 clear
 restart_system() {
-USRSC=$(wget -qO- https://raw.githubusercontent.com/kjsstore/izin/main/ip | grep $ipsaya | awk '{print $2}')
-EXPSC=$(wget -qO- https://raw.githubusercontent.com/kjsstore/izin/main/ip | grep $ipsaya | awk '{print $3}')
+USRSC=$(wget -qO- https://raw.githubusercontent.com/NEW-KJSVIP/izin-ip/main/ip | grep $ipsaya | awk '{print $2}')
+EXPSC=$(wget -qO- https://raw.githubusercontent.com/NEW-KJSVIP/izin-ip/main/ip | grep $ipsaya | awk '{print $3}')
 TIMEZONE=$(printf '%(%H:%M:%S)T')
 TEXT="
 <code>────────────────────</code>
@@ -445,7 +445,7 @@ print_success "Password SSH"
 function udp_mini(){
 clear
 print_install "Memasang Service limit Quota"
-wget -q raw.githubusercontent.com/kjsstore/kjs-new/main/Fls/limit.sh && chmod +x limit.sh && ./limit.sh
+wget -q raw.githubusercontent.com/NEW-KJSVIP/kjs-new.v4/main/Fls/limit.sh && chmod +x limit.sh && ./limit.sh
 mkdir -p /usr/local/kyt/
 wget -q -O /usr/local/kyt/udp-mini "${REPO}Fls/udp-mini"
 chmod +x /usr/local/kyt/udp-mini
@@ -751,7 +751,7 @@ rm -rf /usr/local/sbin/*~
 rm -rf /usr/local/sbin/gz*
 rm -rf /usr/local/sbin/*.bak
 rm -rf /usr/local/sbin/m-noobz
-wget https://raw.githubusercontent.com/kjsstore/kjs-new/main/Cfg/m-noobz 
+wget https://raw.githubusercontent.com/NEW-KJSVIP/kjs-new.v4/main/Cfg/m-noobz 
 cp m-noobz /usr/local/sbin
 rm m-noobz*
 chmod +x /usr/local/sbin/m-noobz
